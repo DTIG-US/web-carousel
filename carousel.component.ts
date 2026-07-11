9f80c559930c1d4d98be91627dc2ed74734b5d9a
John Aziz <john.aziz@dtig.us>
2026-07-11 17:53:10 -0500

    [Fixed] No display of items in data.json

--- carousel.component.ts
+++ carousel.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
 import * as siteData from '../../data.json'
+import { CommonModule } from '@angular/common';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
+
 @Component({
   selector: 'app-carousel',
+  standalone: true as boolean,
+  imports: [CommonModule, SlickCarouselModule],
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.css']
 })
